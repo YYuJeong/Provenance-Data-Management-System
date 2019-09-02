1aa358214a1efb3edc24bf1c025aa785de79e1b2
siyeons <cjswotldus04>
2019-09-02 20:21:30 +0900

    add_node 함수에 라벨 추가
    agent(Agent), waw(wasAssociatedWith), wgb(wasGeneratedBy), act(Activity)

--- readData.py
+++ readData.py
@@ -10,16 +10,21 @@ with open("newdata.csv",'r') as f:
 
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "wowhi223"))
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "wowhi223")) #11001
 
 def add_node(tx,receiver_name,sender_name,entity_name, activity_time,activity_price, sender_attribute, receiver_attribute,  entity_use):
-    tx.run("CREATE(: Agent {name: $receiver_name, attribute: $receiver_attribute}) <- [:wasAssociatedWith]-(: Activity { name: 'Own'}) <- [:wasGeneratedBy]-(: Entity { name: $entity_name, use: $entity_use}) - [:wasGeneratedBy] -> (: Activity { name: 'Buy', price: $activity_price,time: $activity_time})- [:wasAssociatedWith] -> (: Agent {name: $sender_name, attribute: $sender_attribute})"
+    tx.run("CREATE(agent: Agent {name: $receiver_name, attribute: $receiver_attribute}) <- [waw:wasAssociatedWith]-(act: Activity { name: 'Own'}) <- [wgb:wasGeneratedBy]-(entity: Entity { name: $entity_name, use: $entity_use}) - [wgb:wasGeneratedBy] -> (act: Activity { name: 'Buy', price: $activity_price,time: $activity_time})- [waw:wasAssociatedWith] -> (agent: Agent {name: $sender_name, attribute: $sender_attribute})"
          ,receiver_name=receiver_name,sender_name=sender_name,entity_name=entity_name,activity_price=activity_price,activity_time=activity_time, sender_attribute=sender_attribute, receiver_attribute=receiver_attribute, entity_use=entity_use)
     #tx.run("CREATE(: Agent {name: $receiver_name}) <- [:wasAssociatedWith]-(: Activity { name: 'Own'}) <- [:wasGeneratedBy]-(: Entity { name: $entity_name}) - [:wasGeneratedBy] -> (: Activity { name: 'Buy', price: $activity_price,time: $activity_time})- [:wasAssociatedWith] -> (: Agent {name: $sender_name})"
         # ,receiver_name=receiver_name,sender_name=sender_name,entity_name=entity_name,activity_price=activity_price,activity_time=activity_time)
 
-
-
+'''
+def add_node(tx,sender_name,receiver_name,entity_name, activity_time,activity_price, sender_attribute, receiver_attribute,  entity_use):
+    tx.run("CREATE(b: Buyer {name: $receiver_name, division: $receiver_attribute}) <- [buy:Buy{date: $activity_time}]-(d: Data { name: $entity_name, price:$activity_price, use:  $entity_use }) <- [sell:Sell{date: $activity_time}] - (s: Seller {name: $sender_name, division: $sender_attribute})"
+         ,receiver_name=receiver_name,sender_name=sender_name,entity_name=entity_name,activity_price=activity_price,activity_time=activity_time, sender_attribute=sender_attribute, receiver_attribute=receiver_attribute, entity_use=entity_use)
+    #tx.run("CREATE(: Agent {name: $receiver_name}) <- [:wasAssociatedWith]-(: Activity { name: 'Own'}) <- [:wasGeneratedBy]-(: Entity { name: $entity_name}) - [:wasGeneratedBy] -> (: Activity { name: 'Buy', price: $activity_price,time: $activity_time})- [:wasAssociatedWith] -> (: Agent {name: $sender_name})"
+        # ,receiver_name=receiver_name,sender_name=sender_name,entity_name=entity_name,activity_price=activity_price,activity_time=activity_time)
+'''
         
 
 with driver.session() as session:
@@ -31,20 +36,25 @@ with driver.session() as session:
   
 # Bi-directional relationship?
 #u1.relationships.create("friends", u2)
-'''
+
 def merge(tx):
-   tx.run("MATCH (agent:Agent) WITH agent.name AS agent, collect(agent) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
+   tx.run("MATCH (s:Seller) WITH s.name AS s, collect(s) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
 
 with driver.session() as session:
           session.read_transaction(merge)
 
 def merge1(tx):
-   tx.run("MATCH (entity:Entity) WITH entity.name AS entity, collect(entity) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
+   tx.run("MATCH (d:Data) WITH d.name AS d, collect(d) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
 
 with driver.session() as session:
           session.read_transaction(merge1)
                                                     
-'''
+def merge2(tx):
+   tx.run("MATCH (b:Buyer) WITH b.name AS b, collect(b) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
+
+with driver.session() as session:
+          session.read_transaction(merge2)
+
 
 
 print("start_time", start_time)
