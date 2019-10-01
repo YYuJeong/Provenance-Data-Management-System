a81b710594f0201ed71f765f4959716c8f1fe5d8
Siyeon Lee <35549653+siyeons@users.noreply.github.com>
2019-10-01 13:05:38 +0900

    read data 업데이트, json, json intersection

--- read.py
+++ read.py
@@ -9,7 +9,7 @@ from neo4j import GraphDatabase
 with open("randomData.csv",'r') as f:
     matrix = list(csv.reader(f,delimiter=","))
     
-    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "wowhi223"))
+    driver = GraphDatabase.driver("bolt://localhost:11002", auth=("neo4j", "wowhi223"))
           
     #create agent
     def create_agent(tx, name, affiliation):
@@ -17,32 +17,32 @@ with open("randomData.csv",'r') as f:
     
     #create activity
     def create_activity(tx, activityType, date):
-        tx.run("CREATE (activity:Activity {activityType: $activityType, date: $date})", activityType=activityType, date=date)
+        tx.run("CREATE (activity:Activity {name: $activityType, date: $date})", activityType=activityType, date=date)
     
   
     #create entity
     def create_entity(tx, dataName, dataType, price, device):
-        tx.run("CREATE (entity:Entity {dataName: $dataName, dataType: $dataType, price: $price, device: $device})", dataName=dataName, dataType=dataType, price=price, device=device)
+        tx.run("CREATE (entity:Entity {name: $dataName, type: $dataType, price: $price, device: $device})", dataName=dataName, dataType=dataType, price=price, device=device)
     
   
     #create a relationship between entity and agent - wasAttributedTo
     def wasAttributedTo(tx, name, affiliation, dataName, dataType, price, device):
-        tx.run("MATCH (agent:Agent {name:$name, affiliation:$affiliation}) "
-               "MATCH (entity:Entity {dataName: $dataName, dataType: $dataType, price: $price, device: $device}) "
+        tx.run("MATCH (agent:Agent {name: $name, affiliation: $affiliation}) "
+               "MATCH (entity:Entity {name: $dataName, type: $dataType, price: $price, device: $device}) "
                "MERGE (agent)-[:wasAttributedTo]->(entity)",
                name=name, affiliation=affiliation, dataName=dataName, dataType=dataType, price=price, device=device)
     
 
     #create a relationship between activity and entity - wasGeneratedBy
     def wasGeneratedBy(tx, dataName, dataType, price, device, activityType, date):
-        tx.run("MATCH (entity: Entity {dataName: $dataName, dataType: $dataType, price: $price, device: $device}) "
-               "MATCH (activity: Activity {activityType: $activityType, date: $date})"
+        tx.run("MATCH (entity: Entity {name: $dataName, type: $dataType, price: $price, device: $device}) "
+               "MATCH (activity: Activity {name: $activityType, date: $date})"
                "MERGE (entity)-[:wasGeneratedBy]->(activity)",     
                dataName=dataName, dataType=dataType, price=price, device=device, activityType=activityType, date=date)
 
     #create a relationship between activity and agent - wasAssociatedWith
     def wasAssociatedWith(tx, activityType, date, name, affiliation):
-        tx.run("MATCH (activity: Activity {activityType: $activityType, date: $date}) "
+        tx.run("MATCH (activity: Activity {name: $activityType, date: $date}) "
                "MATCH (agent: Agent {name: $name, affiliation: $affiliation})"
                "MERGE (activity)-[:wasAssociatedWith]->(agent)",
                activityType=activityType, date=date, name=name, affiliation=affiliation)
@@ -50,8 +50,8 @@ with open("randomData.csv",'r') as f:
   
     #create a relationship between entity and activity - used
     def used(tx, activityType, date, dataName, dataType, price, device):
-        tx.run("MATCH (activity: Activity {activityType: $activityType, date: $date}) "
-               "MATCH (entity:Entity {dataName: $dataName, dataType: $dataType, price:$price, device: $device}) "
+        tx.run("MATCH (activity: Activity {name: $activityType, date: $date}) "
+               "MATCH (entity:Entity {name: $dataName, type: $dataType, price:$price, device: $device}) "
                "MERGE (entity)<-[:used]-(activity)", 
                 activityType=activityType, date=date, dataName=dataName, dataType=dataType, price=price, device=device)
      
@@ -69,10 +69,10 @@ with open("randomData.csv",'r') as f:
 
 
     def merge1(tx):
-       tx.run("MATCH (ac:Activity) WITH ac.activityType AS ac, collect(ac) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
+       tx.run("MATCH (ac:Activity) WITH ac.name AS ac, collect(ac) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
   
     def merge2(tx):
-       tx.run("MATCH (e:Entity) WITH e.dataName AS e, collect(e) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
+       tx.run("MATCH (e:Entity) WITH e.name AS e, collect(e) as node2Merge WITH node2Merge, extract(x IN node2Merge | x.match) AS matches CALL apoc.refactor.mergeNodes(node2Merge) yield node RETURN *")
 
 
 
@@ -83,7 +83,7 @@ with open("randomData.csv",'r') as f:
             activityType = matrix[i][2]
             date = matrix[i][3]
             dataName = matrix[i][4]
-            dataType = matrix[i][5]
+            dataType = matrix[i][5]             
             price = matrix[i][6]
             device = matrix[i][7]
                 
