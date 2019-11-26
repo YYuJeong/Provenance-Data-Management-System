4df7bb43f8098af4ef13fc15708a5c5ef6295a19
YangYuJeong <diddbwjd96@sookmyung.ac.kr>
2019-11-26 16:58:39 +0900

    - algorithm

--- Keyword Search/KeywordSearch.py
+++ Keyword Search/KeywordSearch.py
@@ -47,6 +47,13 @@ def subgraphG(nodes):
     g = nodes
     return g
 
+def shortestPath(tx, n1, n2):
+    length = (tx.run("MATCH (k1:"+next(iter(n1[0].labels))+"{ name: $k1_name, affiliation: $k1_aff }),(k2: " + next(iter(n2[0].labels))+ " { name: $k2_name, affiliation : $k2_aff }), "
+                    "p = shortestPath((k1)-[*]-(k2)) "
+                    "RETURN p, length(p)" 
+                    , k1_name = n1[0]["name"], k1_aff = n1[0]["affiliation"]
+                    , k2_name = n2[0]["name"], k2_aff = n2[0]["affiliation"])).values()[0]
+    return length
 
     
 with driver.session() as session:
@@ -54,12 +61,59 @@ with driver.session() as session:
     k2Label = session.read_transaction(check_nodeLabel,  keyword= '서민지')    
     k1nodes = session.read_transaction(get_nodes, keyword= '양유정', nodeLabel = k1Label)
     k2nodes = session.read_transaction(get_nodes, keyword= '서민지', nodeLabel = k2Label)
-
+    print("")
     #initialize subgraph g and N
     g = []
-    N = k1nodes
-    for node in k2nodes:
-        N.append(node)
-    print("g : ", g)
-    print("N : ", N)
+    N = k1nodes + k2nodes
+    graphG = [] # pair 저장
+    
+    #algorithm
+    g.append(N[0])
+    del N[0]
+    
+    for node in g:
+        print("g :", node)
+    print("")
+    for node in N:
+        print("N :", node)
+    print("=="*50)
+    print("")
+    for k in range(len(k1nodes)+len(k2nodes)-1):
+        print("========================================", k, " 번째 단계 ===========================================")
+        for j in range(len(g)):
+            path = []
+            pathLen = []
+            if len(N) != 0:
+                for i in range(len(N)):
+                    print("g[", j, "]: ",g[j])
+                    print("N[", i, "]: ",N[i])
+                    print("")
+                    path.append(session.read_transaction(shortestPath, n1 = g[j], n2 = N[i])[0])
+                    pathLen.append(session.read_transaction(shortestPath, n1 = g[j], n2 = N[i])[1])
+                
+                print("pathLen: " , pathLen)
+                shortestLenIndex = pathLen.index(min(pathLen))
+                print("shortestLenIndex: ", shortestLenIndex )
+                graphG.append(path[shortestLenIndex])
+                g.append(N[shortestLenIndex])
+                del N[shortestLenIndex]
+                print("")
+                for node in g:
+                    print("g :", node)
+                print("")
+                for node in N:
+                    print("N :", node)
+                
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
