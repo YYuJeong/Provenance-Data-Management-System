e70ec749f02b83141e502aebcce1e6df835d0270
YYuJeong <a92983469@gmail.com>
2019-12-04 15:35:11 +0900

    - random data generate

--- neo/app.js
+++ neo/app.js
@@ -41,6 +41,7 @@ var fileuploadRouter = require('./routes/data/uploadData');
 var ejs = require('ejs');
 var app = express();
 
+
 var neo4j = require('neo4j-driver').v1;
 var driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'wowhi223'));
 var session = driver.session();
