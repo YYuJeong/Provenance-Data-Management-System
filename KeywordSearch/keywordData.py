4ccb10aae86704278ea5c4b1202dc8222934a520
YYuJeong <a92983469@gmail.com>
2019-12-04 17:02:48 +0900

    no message

--- KeywordSearch/keywordData.py
+++ KeywordSearch/keywordData.py
@@ -88,15 +88,13 @@ def delete_duplRelation(tx):
 with driver.session() as session:
     for i in range(len(matrix)):
         session.write_transaction(add_node,matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5],matrix[i][6],matrix[i][7], matrix[i][8],matrix[i][9],matrix[i][10],matrix[i][11], matrix[i][12],matrix[i][13])
-<<<<<<< HEAD
-=======
 
         #session.write_transaction(add_node,matrix[0][0],matrix[0][1],matrix[0][2],matrix[0][3],matrix[0][4],matrix[0][5],matrix[0][6],matrix[0][7], matrix[0][8],matrix[0][9],matrix[0][10],matrix[0][11], matrix[0][12],matrix[0][13])
 
     #    for i in range(6):
 #        session.write_transaction(add_node,matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5],matrix[i][6],matrix[i][7], matrix[i][8],matrix[i][9],matrix[i][10],matrix[i][11], matrix[i][12],matrix[i][13])
 
->>>>>>> df714e202e36481ce27e6d6bd520db1abbeda7d9
+
     session.read_transaction(merge_data)
     session.read_transaction(merge_person)
     session.read_transaction(delete_duplRelation)
