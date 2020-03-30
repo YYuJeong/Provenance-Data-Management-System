cb85c3b1aa4476623512a8326e955be2f75c8871
JooyeonLee <pbol26@gmail.com>
2020-03-30 22:46:41 +0900

    keyword result

--- neo/public/scripts/cy2neod3.js
+++ neo/public/scripts/cy2neod3.js
@@ -43,12 +43,13 @@ function Cy2NeoD3(config, graphId, tableId, sourceId, execId, urlSource, renderG
             }
             return false;
         }}*/
-
+    
+    /*
     $("#" + execId).click(function (evt) {
         try {
             evt.preventDefault();
             var query = editor;
-            //console.log("Executing Query",query);
+            console.log("Executing Query",query);
 
             neo.executeQuery(query, {}, function (err, res) {
                 res = res || {}
@@ -83,6 +84,45 @@ function Cy2NeoD3(config, graphId, tableId, sourceId, execId, urlSource, renderG
         }
         return false;
     });
+    */
+
+   try {
+    //evt.preventDefault();
+    var query = editor;
+    console.log("Executing Query",query);
+
+    neo.executeQuery(query, {}, function (err, res) {
+        res = res || {}
+        var graph = res.graph;
+        if (renderGraph) {
+            if (graph) {
+                var c = $("#" + graphId);
+                var t = $("#" + tableId);
+                t.empty();
+                c.empty();
+                neod3.render(graphId, c, graph);
+                renderResult(tableId, res.table);
+                console.log(graph)
+            } else {
+                if (err) {
+                    console.log(err);
+                    if (err.length > 0) {
+                        sweetAlert("Cypher error", err[0].code + "\n" + err[0].message, "error");
+                    } else {
+                        sweetAlert("Ajax " + err.statusText, "Status " + err.status + ": " + err.state(), "error");
+                    }
+                }
+            }
+        }
+        if (cbResult) {
+            cbResult(res);
+        }
+    });
+} catch (e) {
+    console.log(e);
+    sweetAlert("Catched error", e, "error");
+}
+
 }
 
 exports.Cy2NeoD3 = Cy2NeoD3;
