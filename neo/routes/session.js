be7d72773d687fbd4dffc5fbad2bf05b9b772b89
JooyeonLee <pbol26@gmail.com>
2020-05-10 04:20:57 +0900

    modify login/contact

--- neo/routes/session.js
+++ neo/routes/session.js
@@ -1,29 +1,21 @@
 let session_info ={
 
-    
-    email: 'sw1234',
-    user: '이상우',
-    pid: '980514-1520414',
-    gubun: '사용자',
-    password: 'sw1234',
-    authenticated:true,
-    /*
     email: '',
     user: '',
-    gubun: '',
     password: '',
-    authenticated:false,
+    pid: '',
+    gubun: '',
+    authenticated: false,
     message: ''
-    */
 };
 
-function setSession(email, user, pid, gubun, password, authenticated, message){
+function setSession(email, user, password, pid, gubun, authenticated, message){
     session_info ={
         email: email,
         user: user,
+        password: password,
         pid: pid,
         gubun: gubun,
-        password: password,
         authenticated: authenticated,
     };
 }
