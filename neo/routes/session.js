6ebd4976b16a721451df03897af4d86794f48026
JooyeonLee <pbol26@gmail.com>
2020-05-12 17:24:52 +0900

    no message

--- neo/routes/session.js
+++ neo/routes/session.js
@@ -1,11 +1,11 @@
 let session_info ={
 
-    email: 'sw1234',
-    user: '이상우',
-    password: '1234',
-    pid: '980514-1520414',
-    gubun: '사용자',
-    authenticated: true,
+    email: '',
+    user: '',
+    password: '',
+    pid: '',
+    gubun: '',
+    authenticated: false,
     message: ''
 };
 
